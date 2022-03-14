b44521cbb070dfd1a8d34c03ac8aad8685c70188
GarciaAlegria <84537086+GarciaAlegria@users.noreply.github.com>
2022-03-14 14:41:09 -0600

    Creación de clase

---
+++ Ejercicio5.py
@@ -0,0 +1,6 @@
+#El siguiente programa creado fue tomado de
+#Referencia: en clase https://uvg.instructure.com/courses/23918/files/3721700
+
+#Aqui se importaran los paquetes
+import simpy
+import random
